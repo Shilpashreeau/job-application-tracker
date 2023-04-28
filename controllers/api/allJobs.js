ad3ed62525c0788e9de699fbfb3d3e88e2c70aae
Shilpashreeau <shilpashreehebbar@gmail.com>
2023-04-28 11:14:36 -0400

    created job schema

---
+++ controllers/api/allJobs.js
@@ -0,0 +1,26 @@
+const Job = require('../../models/job');
+
+module.exports = {
+  index,
+  show
+};
+
+async function index(req, res) {
+  try{
+    const items = await Job.find({}).sort('name').exec();
+   
+   
+    res.status(200).json(items);
+  }catch(e){
+    res.status(400).json({ msg: e.message });
+  }
+}
+
+async function show(req, res) {
+  try{
+    const item = await Job.findById(req.params.id);
+    res.status(200).json(item);
+  }catch(e){
+    res.status(400).json({ msg: e.message });
+  }  
+}
